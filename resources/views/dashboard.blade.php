5d75071f77bbf7d7c5e030cf5d7cb585fa4221a9
Faviana Sanchez <sanchezfaviana29@gmail.com>
2024-02-27 12:58:52 -0300

    Foodie Manager

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Foodie Restaurant Software</title>
+    <link rel='stylesheet' href='./estilo.css'>
+</head>
+<body> 
+<div class='formulario'>
+<h1>Inicio de sesión</h1>
+        <form method='post'>
+        <div class='username'>
+        <input type='text' name='username'required>
+        <label>Nombre de usuario</label>
+</div>
+<div class='contraseña'>
+    <input type='password' name='password' required>
+    <label>Contraseña</label>
+</div>
+<div>
+<input type='submit' value='Iniciar sesion'>
+</div> 
+</form>
+</div>
+</body>
+</html>
